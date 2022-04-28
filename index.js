ad8e57fd8e000fdfe91c94a468e28cad292e4702
Marcus <marcus.chong@trentglobal.edu.sg>
2022-04-28 08:34:10 +0000

    added flash message and session dependencies

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const hbs = require("hbs");
 const wax = require("wax-on");
 require("dotenv").config();
 
+const session = require("express-session");
+const flash = require("connect-flash");
+const FileStore = require("session-file-store")(session);
+
 // create an instance of express app
 let app = express();
 
@@ -16,24 +20,41 @@ app.use(express.static("public"));
 wax.on(hbs.handlebars);
 wax.setLayoutPath("./views/layouts");
 
+app.use(flash());
+
 // enable forms
 app.use(
   express.urlencoded({
-    extended: false
+    extended: false,
   })
 );
 
-const landingRoutes = require('./routes/landing');
-const cakeRoutes = require('./routes/cakes')
-const productRoutes = require('./routes/products')
+const landingRoutes = require("./routes/landing");
+const cakeRoutes = require("./routes/cakes");
+const productRoutes = require("./routes/products");
 async function main() {
-  app.use('/',landingRoutes)
-  app.use('/cakes', cakeRoutes);
-  app.use('/products', productRoutes)
+  // set up sessions
+  app.use(
+    session({
+      store: new FileStore(),
+      secret: "keyboard cat",
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
+  // Register Flash middleware
+  app.use(function (req, res, next) {
+    res.locals.success_messages = req.flash("success_messages");
+    res.locals.error_messages = req.flash("error_messages");
+    next();
+  });
+  app.use("/", landingRoutes);
+  app.use("/cakes", cakeRoutes);
+  app.use("/products", productRoutes);
 }
 
 main();
 
-app.listen(3001, () => {
+app.listen(3000, () => {
   console.log("Server has started");
 });
--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Cake, Product, Cakesize } = require("../models");
-const { bootstrapField, createProductForm } = require('../forms');
+const { bootstrapField, createProductForm } = require("../forms");
 
 async function allCakes() {
   return await Cake.fetchAll().map((cake) => {
@@ -17,111 +17,132 @@ async function allSize() {
 // CRUD - READ
 router.get("/", async (req, res) => {
   let products = await Product.collection().fetch({
-    withRelated:['cake.season', 'cakesize', 'cake.ingredients'],
+    withRelated: ["cake.season", "cakesize", "cake.ingredients"],
   });
   res.render("products/index.hbs", {
-    'products': products.toJSON()
+    products: products.toJSON(),
   });
 });
 
 // CRUD - CREATE
-router.get('/create', async (req, res) => {
-  const productForm = createProductForm(await allCakes(), await allSize())
-  res.render('products/create.hbs',{
-      'form': productForm.toHTML(bootstrapField)
-  })
-})
-router.post('/create', async(req,res)=>{
+router.get("/create", async (req, res) => {
+  const productForm = createProductForm(await allCakes(), await allSize());
+  res.render("products/create.hbs", {
+    form: productForm.toHTML(bootstrapField),
+  });
+});
+router.post("/create", async (req, res) => {
   // during the post, there needs to be a check for repeated product for variation size
   let products = await Product.collection().fetch();
   const productForm = createProductForm(await allCakes(), await allSize());
   productForm.handle(req, {
-      'success': async (form) => {
-          for (let oneProduct of products.toJSON()){
-            console.log(form.fields.cake_id.value)
-            console.log(oneProduct.cake_id)
-            if (productForm.fields.cake_id.value == oneProduct.cake_id){
-              if (productForm.fields.cakesize_id.value == oneProduct.cakesize_id){
-                console.log("I HAVE ENTERED THE ERROR")
-                res.render('products/create.hbs', {
-                  'form': form.toHTML(bootstrapField)
-              })
-              }
-            }
+    success: async (form) => {
+      for (let oneProduct of products.toJSON()) {
+        if (form.fields.cake_id.value == oneProduct.cake_id) {
+          if (form.fields.cakesize_id.value == oneProduct.cakesize_id) {
+            req.flash("error_messages", `This product is already existing within database`);
+            res.redirect("/products/create");
+            return;
           }
-          const product = new Product(form.data);
-          await product.save();
-          res.redirect('/products');
-      },
-      'error': async (form) => {
-          res.render('products/create.hbs', {
-              'form': form.toHTML(bootstrapField)
-          })
+        }
       }
-  })
-})
+      const product = new Product(form.data);
+      console.log(product.toJSON())
+      await product.save();
+
+      const fetchProduct = await Product.where({
+        id: form.fields.cake_id.value,
+      }).fetch({
+        require: true,
+        withRelated: ["cake", "cakesize"]
+      });
+      req.flash("success_messages", `New Product ${(fetchProduct.toJSON()).cake.name}, ${(fetchProduct.toJSON()).cakesize.size} has been created`)
+      res.redirect("/products");
+    },
+    error: async (form) => {
+      req.flash("error_messages", `Creation error. Try again`);
+      res.render("products/create.hbs", {
+        form: form.toHTML(bootstrapField),
+      });
+    },
+  });
+});
 
 // CRUD - UPDATE
-router.get('/:product_id/update', async (req, res) => {
-  const productId = req.params.product_id
+router.get("/:product_id/update", async (req, res) => {
+  const productId = req.params.product_id;
   const product = await Product.where({
-      'id': productId
+    id: productId,
   }).fetch({
-      require: true
+    require: true,
   });
 
   const productForm = createProductForm(await allCakes(), await allSize());
-  productForm.fields.cake_id.value = product.get('cake_id');
-  productForm.fields.cakesize_id.value = product.get('cakesize_id');
-  productForm.fields.price.value = product.get('price');
-  res.render('products/update.hbs', {
-      'form': productForm.toHTML(bootstrapField),
-      'product': product
-  })
-})
-router.post('/:product_id/update', async (req, res) => {
+  productForm.fields.cake_id.value = product.get("cake_id");
+  productForm.fields.cakesize_id.value = product.get("cakesize_id");
+  productForm.fields.price.value = product.get("price");
+  res.render("products/update.hbs", {
+    form: productForm.toHTML(bootstrapField),
+    product: product,
+  });
+});
+router.post("/:product_id/update", async (req, res) => {
+  // during the post, there needs to be a check for repeated product for variation size
+  let products = await Product.collection().fetch();
   const product = await Product.where({
-      'id': req.params.product_id
+    id: req.params.product_id,
   }).fetch({
-      required: true
+    required: true,
   });
   const productForm = createProductForm(await allCakes(), await allSize());
   productForm.handle(req, {
-      'success': async (form) => {
-          product.set(form.data);
-          product.save();
-          res.redirect('/products');
-      },
-      'error': async (form) => {
-          res.render('products/update', {
-              'form': form.toHTML(bootstrapField)
-          })
+    success: async (form) => {
+      for (let oneProduct of products.toJSON()) {
+        if (form.fields.cake_id.value == oneProduct.cake_id) {
+          if (form.fields.cakesize_id.value == oneProduct.cakesize_id) {
+            req.flash("error_messages", `This product is already existing within database`);
+            res.redirect("/products");
+            return;
+          }
+        }
       }
-  })
-})
+      product.set(form.data);
+      product.save();
+      req.flash("success_messages", `Product has been updated`);
+      res.redirect("/products");
+    },
+    error: async (form) => {
+      req.flash("error_messages", `Update error. Try again`);
+      res.render("products/update", {
+        form: form.toHTML(bootstrapField),
+      });
+    },
+  });
+});
 
 // CRUD - DELETE
-router.get('/:product_id/delete', async(req,res)=>{
+router.get("/:product_id/delete", async (req, res) => {
   // fetch the product that we want to delete
   const product = await Product.where({
-      'id': req.params.product_id
+    id: req.params.product_id,
   }).fetch({
-      require: true
+    require: true,
   });
-  res.render('products/delete.hbs', {
-      'product': product.toJSON()
-  })
+  res.render("products/delete.hbs", {
+    product: product.toJSON(),
+  });
 });
-router.post('/:product_id/delete', async(req,res)=>{
+router.post("/:product_id/delete", async (req, res) => {
   const product = await Product.where({
-      'id': req.params.product_id
+    id: req.params.product_id,
   }).fetch({
-      require: true
+    require: true,
+    withRelated: ["cake", "cakesize"]
   });
+  console.log(product.toJSON())
   await product.destroy();
-  res.redirect('/products')
-})
-
-
+  req.flash("success_messages", `Product ${(product.toJSON()).cake.name}, ${(product.toJSON()).cakesize.size}cm has been deleted`);
+  res.redirect("/products");
+});
 
 module.exports = router;
